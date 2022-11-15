cc36e99d3ad4dcc92a44cbe05eeded6372827d50
Osmael Sousa <osmaelsousa.ti@gmail.com>
2022-11-15 16:57:09 -0300

    get, post de user e post de todos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,67 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
+
+  const {name, username} = request.body;
+
+  const user = { 
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: []
+  }
+  
+  users.push(user);
+
+  return response.status(201).json(user);
+});
+
+app.get('/users', (request, response) => {
+
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  return response.json(user);
+});
+
+app.get('/todos', (request, response) => {
   // Complete aqui
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+app.post('/todos', (request, response) => {
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {title, deadline} = request.body;
+  
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  const todo = {
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
